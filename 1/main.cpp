8afd5aaa14f43078bbb65dbea457dedff38fd10d
Ivan Baksheev <zpix-dev@list.ru>
2021-09-06 18:58:39 +0700

    lab1: timeout done

--- 1/main.cpp
+++ 1/main.cpp
@@ -8,21 +8,55 @@
 #include <cstring>
 #include <iostream>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <netdb.h>
-#include <stdio.h>
+#include <unordered_map>
+#include <algorithm>
+
+const int remove_timeout = 2000;
+const int timeout = 1000;
+
 #define PORT     8080
 #define MAXLINE 1024
 
 void send_info_about_me(const std::string& info, int sockfd, struct sockaddr *cliaddr_ptr, const size_t cliaddr_len) {
-    std::cout << "sending info about me" << std::endl;
+//    std::cout << "sending info about me" << std::endl;
     sendto(sockfd, info.c_str(), info.length(), 0, cliaddr_ptr, cliaddr_len);
 }
 
+class DB {
+    std::unordered_map<std::string, time_t> map;
+
+public:
+    void add(const std::string& s) {
+        map[s] = time(0);
+    }
+
+    int clear() {
+        const time_t now = time(0);
+        return std::erase_if(map, [&now](const auto& item) {
+            return now - item.second >= remove_timeout;
+        });
+    }
+
+    void print() {
+        std::cout << "[" << std::endl;
+        for (const auto& res: map) {
+            std::cout << "\t" << res.first << " = " << res.second << "," << std::endl;
+        }
+        std::cout << "]" << std::endl;
+    }
+
+    bool exists(const std::string& s) {
+        return map.find(s) != map.end();
+    }
+};
+
 int main(int argc, char **argv) {
+    DB db{};
     srand(time(0));
-    const std::string MYSELF_ID = "UID-"+std::to_string(rand());
-    std::cout << "MYSELF is" << MYSELF_ID << std::endl;
+
+    const std::string MYSELF_ID = "UID-" + std::to_string(rand());
+    std::cout << "MYSELF_ID is " << MYSELF_ID << std::endl;
 
     struct sockaddr_in servaddr{}, cliaddr{};
     struct sockaddr_in broadcastaddr{};
@@ -65,7 +99,7 @@ int main(int argc, char **argv) {
     send_info_about_me(MYSELF_ID, sockfd, reinterpret_cast<sockaddr *>(&broadcastaddr), sizeof(broadcastaddr));
 
     do {
-        std::cout << "polling..." << std::endl;
+//        std::cout << "polling..." << std::endl;
         int res = poll(&fd, 1, 1000);
 
         if (res < 0) {
@@ -74,7 +108,6 @@ int main(int argc, char **argv) {
         }
 
         if (res == 0) {
-            std::cout << "timeout" << std::endl;
             send_info_about_me(MYSELF_ID, sockfd, reinterpret_cast<sockaddr *>(&broadcastaddr), sizeof(broadcastaddr));
         } else {
             if (fd.revents != POLLIN) {
@@ -84,7 +117,8 @@ int main(int argc, char **argv) {
             char buffer[MAXLINE];
             const int len = sizeof(cliaddr);
 
-            ssize_t read = recvfrom(sockfd, (char *) buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, (socklen_t *) &len);
+            ssize_t read = recvfrom(sockfd, (char *) buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr,
+                                    (socklen_t *) &len);
 
             if (read < 0) {
                 perror("recvfrom");
@@ -93,13 +127,13 @@ int main(int argc, char **argv) {
 
             buffer[read] = '\0';
             std::string client_data = buffer;
-            if (client_data == MYSELF_ID) {
-                std::cout << "already me" << std::endl;
-                continue;
-            }
+//            if (client_data == MYSELF_ID) {
+//                std::cout << "already me" << std::endl;
+//                continue;
+//            }
 
             char host[NI_MAXHOST];
-            if (getnameinfo((sockaddr*)&cliaddr, len,
+            if (getnameinfo((sockaddr *) &cliaddr, len,
                             host, NI_MAXHOST, // to try to get domain name don't put NI_NUMERICHOST flag
                             nullptr, 0,          // use char serv[NI_MAXSERV] if you need port number
                             NI_NUMERICHOST    // | NI_NUMERICSERV
@@ -110,7 +144,13 @@ int main(int argc, char **argv) {
                 client_data += "-" + std::string(host);
             }
 
-            std::cout << "got info: " << client_data << cliaddr.sin_port << std::endl;
+//            std::cout << "got info: " << client_data << ":" << cliaddr.sin_port << std::endl;
+            db.clear();
+            int exists = db.exists(client_data);
+            if (!exists) {
+                db.add(client_data);
+                db.print();
+            }
         }
     } while (true);
 
