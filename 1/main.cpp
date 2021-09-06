a7c46bf532db0754e5ecff3f80be1ea6431aa955
Ivan Baksheev <zpix-dev@list.ru>
2021-09-06 21:55:56 +0700

    lab1: pre rewrite

--- 1/main.cpp
+++ 1/main.cpp
@@ -64,6 +64,7 @@ public:
     }
 };
 
+
 int main(int argc, char **argv) {
     DB db{};
     srand(time(0));
@@ -71,69 +72,90 @@ int main(int argc, char **argv) {
     const std::string MYSELF_ID = "UID-" + std::to_string(rand());
     std::cout << "MYSELF_ID is " << MYSELF_ID << std::endl;
 
+    int input_sock, output_sock;
     int trueflag = 1;
     int falseflag = 1;
-    struct sockaddr_in servaddr{}, cliaddr{};
+    {
+        if ((input_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (ioctl(input_sock, FIONBIO, (char *) &trueflag) < 0) {
+            perror("socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (setsockopt(input_sock, SOL_SOCKET, SO_REUSEADDR, &trueflag, sizeof trueflag) < 0) {
+            perror("setsockopt reuseaddr failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (setsockopt(input_sock, SOL_SOCKET, SO_REUSEPORT, &trueflag, sizeof trueflag) < 0) {
+            perror("setsockopt resuseport failed");
+            exit(EXIT_FAILURE);
+        }
+
+        struct ip_mreq mreq{};
+        mreq.imr_multiaddr.s_addr = inet_addr(argv[1]);
+        mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+        if (setsockopt(input_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+            perror("IP_ADD_MEMBERSHIP failed");
+            exit(EXIT_FAILURE);
+        }
+
+        struct sockaddr_in servaddr{};
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(PORT);
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        if (bind(input_sock, (const struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
 
     struct sockaddr_in broadcastaddr{};
-    broadcastaddr.sin_addr.s_addr = inet_addr("225.1.1.1");
     broadcastaddr.sin_family = AF_INET;
+    broadcastaddr.sin_addr.s_addr = inet_addr(argv[1]);
     broadcastaddr.sin_port = htons(PORT);
+    {
+        if ((output_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("output socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+        if (setsockopt(output_sock, SOL_SOCKET, SO_REUSEADDR, &trueflag, sizeof trueflag) < 0) {
+            perror("setsockopt reuseaddr failed");
+            exit(EXIT_FAILURE);
+        }
+        if (setsockopt(output_sock, SOL_SOCKET, SO_REUSEPORT, &trueflag, sizeof trueflag) < 0) {
+            perror("setsockopt resuseport failed");
+            exit(EXIT_FAILURE);
+        }
+        if (setsockopt(output_sock, IPPROTO_IP, IP_MULTICAST_LOOP, (char *) &falseflag, sizeof(falseflag)) < 0) {
+            perror("setsockopt reuseaddr failed");
+            exit(EXIT_FAILURE);
+        }
+        struct in_addr localInterface;
+        localInterface.s_addr = htonl(INADDR_ANY);
+        if (setsockopt(output_sock, IPPROTO_IP, IP_MULTICAST_IF, (char *) &localInterface, sizeof(localInterface)) <
+            0) {
+            perror("setting local interface");
+            exit(1);
+        }
 
-    int sockfd;
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    if (ioctl(sockfd, FIONBIO, (char *) &trueflag) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &trueflag, sizeof trueflag) < 0) {
-        perror("setsockopt reuseaddr failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &trueflag, sizeof trueflag) < 0) {
-        perror("setsockopt resuseport failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &trueflag, sizeof(trueflag)) < 0) {
-        perror("setsockopt broadcast failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_LOOP, &falseflag, sizeof(falseflag)) < 0) {
-        perror("IP_MULTICAST_LOOP failed");
-        exit(EXIT_FAILURE);
-    }
-
-    struct ip_mreq mreq{};
-    mreq.imr_multiaddr.s_addr = inet_addr(argv[1]);
-    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-
-    if (setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
-        perror("IP_ADD_MEMBERSHIP failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (bind(sockfd, (const struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+        if (bind(output_sock, (const struct sockaddr *) &broadcastaddr, sizeof(broadcastaddr)) < 0) {
+            perror("output bind failed");
+            exit(EXIT_FAILURE);
+        }
     }
 
     struct pollfd fd{
-            .fd = sockfd,
+            .fd = input_sock,
             .events = POLLIN
     };
 
-    send_info_about_me(MYSELF_ID, sockfd, reinterpret_cast<sockaddr *>(&broadcastaddr), sizeof(broadcastaddr));
+    send_info_about_me(MYSELF_ID, output_sock, reinterpret_cast<sockaddr *>(&broadcastaddr), sizeof(broadcastaddr));
 
     do {
         bool updated;
@@ -152,14 +174,15 @@ int main(int argc, char **argv) {
             }
 
             char buffer[MAXLINE];
+            struct sockaddr_in cliaddr{};
             const int len = sizeof(cliaddr);
 
-            ssize_t read = recvfrom(sockfd, (char *) buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, (socklen_t *) &len);
+            ssize_t read = recvfrom(input_sock, (char *) buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr,
+                                    (socklen_t *) &len);
 
             if (read < 0) {
                 perror("recvfrom");
                 exit(1);
-                continue;
             }
 
             buffer[read] = '\0';
@@ -181,12 +204,13 @@ int main(int argc, char **argv) {
                 client_data += "-" + std::string(host);
             }
 
-//            std::cout << "got info: " << client_data << ":" << cliaddr.sin_port << std::endl;
+            std::cout << "got info: " << client_data << ":" << cliaddr.sin_port << std::endl;
             updated = updated || !db.exists(client_data);
             db.add(client_data);
+        } else {
+            send_info_about_me(MYSELF_ID, output_sock, reinterpret_cast<sockaddr *>(&broadcastaddr),
+                               sizeof(broadcastaddr));
         }
-
-        send_info_about_me(MYSELF_ID, sockfd, reinterpret_cast<sockaddr *>(&broadcastaddr), sizeof(broadcastaddr));
 
         if (updated) {
             db.print();
