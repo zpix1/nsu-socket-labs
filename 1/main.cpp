e39c9577b95cc38e0852df3bce3cb6d674f7ea63
Ivan Baksheev <zpix-dev@list.ru>
2021-09-06 17:47:51 +0700

    lab1: typo

--- 1/main.cpp
+++ 1/main.cpp
@@ -42,7 +42,7 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &trueflag, sizeof(trueflag)) < 1) {
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &trueflag, sizeof(trueflag)) < 0) {
         perror("setsockopt broadcast failed");
         exit(EXIT_FAILURE);
     }
