8cae0e214c1b5b8ea9382d8d65ebe8d19bd33565
Ivan Baksheev <zpix-dev@list.ru>
2021-11-16 00:05:03 +0700

    lab5: WIP

--- lab5/src/main/java/Socks5Proxy.java
+++ lab5/src/main/java/Socks5Proxy.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
 
 public class Socks5Proxy implements Runnable {
     private static final int BUFFER_SIZE = 1 << 16;
@@ -11,15 +15,50 @@ public class Socks5Proxy implements Runnable {
         public ByteBuffer in;
         public ByteBuffer out;
         public SelectionKey peer;
+        public boolean isDNS;
     }
 
-    public Socks5Proxy(int port, String host) {
+    public Socks5Proxy(String host, int port) {
         this.port = port;
         this.host = host;
     }
 
     @Override
     public void run() {
+        try {
+            var selector = SelectorProvider.provider().openSelector();
+            var serverChannel = ServerSocketChannel.open();
+            serverChannel.configureBlocking(false);
+            serverChannel.socket().bind(new InetSocketAddress(host, port));
+
+            while (true) {
+                selector.select();
+                var iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    var key = iterator.next();
+                    iterator.remove();
+
+                    if (key.isValid()) {
+                        try {
+                            if (key.isAcceptable()) {
+                                accept(key);
+                            } else if (key.isConnectable()) {
+                                connect(key);
+                            } else if (key.isReadable()) {
+                                read(key);
+                            } else if (key.isWritable()) {
+                                write(key);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            close(key);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -31,7 +70,7 @@ public class Socks5Proxy implements Runnable {
         var port = Integer.parseInt(args[0]);
         var host = "127.0.0.1";
 
-        var proxy = new Socks5Proxy(port, host);
+        var proxy = new Socks5Proxy(host, port);
 
         proxy.run();
     }
